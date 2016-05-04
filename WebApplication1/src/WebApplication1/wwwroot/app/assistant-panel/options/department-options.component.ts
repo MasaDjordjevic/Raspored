4ad40534690022680a6b23d1eca3f0321c1e7c1c
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-05-04 09:58:46 +0200

    Chores: Izdvojen .html i .css za Department Options

--- WebApplication1/src/WebApplication1/wwwroot/app/assistant-panel/options/department-options.component.ts
+++ WebApplication1/src/WebApplication1/wwwroot/app/assistant-panel/options/department-options.component.ts
@@ -18,56 +18,11 @@ import {TrimPipe} from "../../pipes/trim.pipe";
 
 @Component({
     selector: 'r-department-options',
-    template: `
-    <div *ngIf="department" style="display: flex; flex-flow: column; height: 100%">
-
-        <h1>{{department.departmentName}}</h1>
-        <dl r-dl>
-            <dt>ID</dt>
-            <dd>{{department.departmentID}}</dd>
-            <dt>Ime</dt>
-            <dd>{{department.departmentName}}</dd>
-            <dt>Godina</dt>
-            <dd>{{department.year}}</dd>
-        </dl>
-
-        <div style="flex: 1 1 auto"></div>
-
-        <r-dialog #newDivisionDialog [source]="newDivisionButton">
-            <r-stepper>
-                <r-step [stepTitle]="'Prvi korak'">
-                    <p>Ovo je prvi korak.</p>
-                </r-step>
-                <r-step [stepTitle]="'Drugi korak'">
-                    <p>Ovo je drugi korak.</p>
-                </r-step>
-                <r-step [stepTitle]="'Treci korak'">
-                    <p>Ovo je treci korak.</p>
-                </r-step>
-            </r-stepper>
-        </r-dialog>
-
-        <r-dialog #debuggingDialog [source]="debuggingButton">
-            <pre style='padding: 3em; width: 60%; font-family: "Source Code Pro", Consolas, Consolas, "Liberation Mono", Menlo, Courier, monospace '>{{department | json | trim}}</pre>
-        </r-dialog>
-
-        <div class="footer-buttons" style="display: flex">
-
-            <button r-button flat #debuggingButton
-                text="JSON" (click)="debuggingDialog.open()">
-            </button>
-
-            <div style="flex: 1 1 auto"></div>
-
-            <button r-button raised #newDivisionButton
-                [text]="'Nova raspodela'"
-                (click)="newDivisionDialog.open()">
-            </button>
-        </div>
-    </div>
-    <p>{{errorMessage}}</p>
-    `,
-    styleUrls: ['app/assistant-panel/options/assistant-panel-options.css'],
+    templateUrl: ['app/assistant-panel/options/department-options.html'],
+    styleUrls: [
+        'app/assistant-panel/options/assistant-panel-options.css',
+        'app/assistant-panel/options/department-options.css'
+    ],
     providers: [DepartmentService],
     directives: [R_DIALOG, R_BUTTON, R_STEPPER, R_DL],
     pipes: [TrimPipe]
