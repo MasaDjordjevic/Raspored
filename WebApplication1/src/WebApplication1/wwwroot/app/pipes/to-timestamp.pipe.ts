45aa792adb3fb549f4b0b4a94e0a57211f58c727
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-05-26 13:27:22 +0200

    Enhancement: Pipe Totimestamp

---
+++ WebApplication1/src/WebApplication1/wwwroot/app/pipes/to-timestamp.pipe.ts
@@ -0,0 +1,16 @@
+import {Pipe, PipeTransform} from "angular2/core";
+
+
+
+@Pipe({
+    name: 'toTimestamp'
+})
+export class ToTimestampPipe implements PipeTransform {
+    transform(min: number) {
+        let hour = Math.floor(min / 60).toString();
+        if (hour.length != 2) hour = "0" + hour;
+        let minute = (min % 60).toString();
+        if (minute.length != 2) minute = "0" + minute;
+        return hour + ":" + minute;
+    }
+}
