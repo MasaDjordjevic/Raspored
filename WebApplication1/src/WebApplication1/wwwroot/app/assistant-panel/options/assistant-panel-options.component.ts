477b057a22cfeec0f49b4c1d8d3c58be0b552e16
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-06-05 09:24:46 +0200

    Preview na pocetnoj stranici nekih komponenti.

--- WebApplication1/src/WebApplication1/wwwroot/app/assistant-panel/options/assistant-panel-options.component.ts
+++ WebApplication1/src/WebApplication1/wwwroot/app/assistant-panel/options/assistant-panel-options.component.ts
@@ -14,32 +14,30 @@ import {multicast} from "rxjs/operator/multicast";
     selector: 'r-assistant-panel-options',
     template: `
     <div style="font-size: 1em; display: flex; justify-content: space-between; flex-wrap: wrap">
-        <!--<r-input class="light-theme" label="Labela" [(val)]="inputText"></r-input> <br/>
-        {{inputText}}
-        <br/> <br/>
-        <r-dropdown [(val)]="dropdown" label="Labela bre">
-            <r-dropdown-item value="b1">boob1</r-dropdown-item>
-            <r-dropdown-item value="b2">boob2</r-dropdown-item>
-            <r-dropdown-item value="b3">boob3</r-dropdown-item>
-            <r-dropdown-item value="b4">boob4</r-dropdown-item>
-        </r-dropdown> <br/>
-        {{dropdown}}
-        <br/>
-        <br/>-->
-        <r-multiple-selector [(val)]="multipleSelector" style="width:45%">
+        <r-input class="light-theme" label="Labela" [(val)]="inputText" style="width:50%;"></r-input>
+        <span>{{inputText}}</span>
+        <div style="width:100%; min-height: 20px;"></div>
+        <r-dropdown [(val)]="dropdown" label="Labela bre" style="width:60%;">
+            <r-dropdown-item *ngFor="let b of ['b1', 'b2', 'b3', 'b4', 'b5']" [value]="b">{{b + "ooo"}}</r-dropdown-item>
+        </r-dropdown>
+        <span>{{dropdown}}</span>
+        <div style="width:100%; min-height: 20px;"></div>
+        <r-multiple-selector [(val)]="multipleSelector" style="width:45%" primaryColor="MaterialGreen">
             <r-multiple-selector-item *ngFor="let n of stuff; let i = index" [val]="i">
                 <span>{{n.split('').reverse().join('')}}</span>
                 <span>{{n.substring(5)}}</span>
             </r-multiple-selector-item>
         </r-multiple-selector>
-        <r-multiple-selector [(val)]="multipleSelector" style="width:45%">
+        <r-multiple-selector [(val)]="multipleSelector" style="width:45%" primaryColor="MaterialRed">
             <r-multiple-selector-item *ngFor="let n of stuff; let i = index" [val]="i">
                 <span>{{n.split('').reverse().join('')}}</span>
                 <span>{{n.substring(5)}}</span>
             </r-multiple-selector-item>
         </r-multiple-selector>
-        <div style="width: 100%; min-height: 40px;">
-        {{multipleSelector | json}}
+        <div style="width:100%; min-height: 20px;"></div>
+        <span>{{multipleSelector | json}}</span>
+        <button (click)="addNew()">toggle 2</button>
+        <div style="width:100%; min-height: 20px;"></div>
     </div>
     `,
     styleUrls: ['app/assistant-panel/options/assistant-panel-options.css'],
@@ -66,8 +64,14 @@ export class AssistantPanelOptionsComponent {
     public randomString = (n) => (Math.random() + 1).toString(36).substring(2, n + 2);
 
     constructor() {
-        for (let i = 0; i < 5; this.stuff[i++] = this.randomString(i * 10 % 7 + 10));
+        for (let i = 0; i < 4; this.stuff[i++] = this.randomString(i * 10 % 7 + 10));
+    }
 
+    public addNew() {
+        this.multipleSelector = this.multipleSelector.concat();
+        var ind = this._multipleSelector.indexOf("2");
+        if (!~ind) this._multipleSelector.push("2");
+        else this._multipleSelector.splice(ind, 1);
     }
 
     assistant: Assistant;
