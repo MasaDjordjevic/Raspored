ed36548ffef0d36aa733f95f72d4af0b12f7be7a
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-06-23 23:47:13 +0200

    Cancel class.

--- WebApplication1/src/WebApplication1/wwwroot/app/assistant-panel/dialogs/Cancel-class.component.ts
+++ WebApplication1/src/WebApplication1/wwwroot/app/assistant-panel/dialogs/Cancel-class.component.ts
@@ -1,11 +1,12 @@
-import {Component, Input, Pipe, PipeTransform, AfterContentInit} from "angular2/core";
+import {Component, Input, Pipe, PipeTransform, AfterContentInit, Output, EventEmitter} from "angular2/core";
 import {TimeSpan} from "../../models/TimeSpan";
 import {GroupsService} from "../../services/groups.service";
 import {R_DROPDOWN} from "../../ui/r-dropdown";
 import {R_INPUT} from "../../ui/r-input-text.component";
 import {R_BUTTON} from "../../ui/r-button.component";
 
-
+import * as moment_ from "moment";
+const moment =  moment_["default"];
 
 
 @Pipe({
@@ -26,19 +27,19 @@ class AddWeeksPipe implements PipeTransform {
 @Component({
     selector: 'cancel-class',
     template: `
-    {{timespan | json}}  
-    <r-dropdown [label]="'Otkazujem čas koji treba da bude održan...'" [(val)]="weekNumber" [primaryColor]="primaryColor">
-        <template ngFor let-i [ngForOf]="[0, 1, 2, 3]">
-            <r-dropdown-item [val]="i">i (ne moze ovo s pajpom)</r-dropdown-item>  
-        </template>  
+    <span *ngIf="dateChoices && dateChoices.length === 1">
+        Otkazivanje časa zakazanog za {{dateChoices[0].format("YYYY-MM-DD")}}.    
+    </span>
+    <r-dropdown *ngIf="dateChoices && dateChoices.length > 1" [label]="'Otkazujem čas koji treba da bude održan...'" [(val)]="weekNumber" [primaryColor]="primaryColor">
+        <r-dropdown-item *ngFor="let date of dateChoices; let i = index" [value]="i">{{date.format("YYYY-MM-DD")}}</r-dropdown-item>  
     </r-dropdown>
 
     <r-input [label]="'Naslov'" [(val)]="title" [primaryColor]="primaryColor"></r-input>
     <textarea [value]="content"></textarea>
    
     <div class="controls">
-        <button r-button flat [text]="'Odustani'" [primaryColor]="primaryColor">Odustani</button>
-        <button r-button raised [text]="'Otkaži čas'" [primaryColor]="primaryColor">Otkaži čas</button>    
+        <button r-button flat [text]="'Odustani'" [primaryColor]="primaryColor" (click)="closeMe()">Odustani</button>
+        <button r-button raised [text]="'Otkaži čas'" [primaryColor]="primaryColor" (click)="cancelClass()">Otkaži čas</button>    
     </div>
     `,
     pipes: [AddWeeksPipe],
@@ -48,27 +49,71 @@ class AddWeeksPipe implements PipeTransform {
 })
 export class CancelClassComponent implements AfterContentInit{
 
-    @Input() primaryColor: string = "MaterialGreen";
+    @Input() primaryColor: string = "MaterialBlue";
     @Input() secondaryColor: string = "MaterialOrange";
 
-    @Input() timespan: any;
-    @Input() groupID: number;
+    @Output() close: EventEmitter<any> = new EventEmitter<any>();
+
+    public closeMe() {
+        this.close.emit("close!");
+    }
+
+    private _timespan: any;
+    @Input() groupId: number;
+
+    @Input() set timespan(timespan) {
+        this._timespan = timespan;
+        this.listDateChoices(timespan.startDate, timespan.period);
+    }
+
+    get timespan() {
+        return this._timespan;
+    }
 
     // Mora da se pozove samo jednom da se Angular ne bi šlogirao
     today = new Date();
 
-    weekNumber:number = 0;
+    weekNumber: number = 0;
 
     constructor( private _service: GroupsService ) {}
 
     title: string = "";
     content: string = "";
+    dateChoices: string[];
 
     ngAfterContentInit() {
 
     }
 
-    cancelClass(value) {
-        this._service.cancelClass(this.groupID, this.title, this.content, this.weekNumber);
+    public listDateChoices(date, period) {
+        var ret: string[] = [];
+        if (period === 0) {
+            // cas se samo jednom odrzava, ponudi mu samo taj jedan izbor
+            ret.push(moment(date).format("YYYY-MM-DD"));
+        } else {
+            // cas se odrzava periodicno, ponudi mu 4 sledece instance casa
+
+            // trazimo koji je dan u nedelji un pitanju: 0 = nedelja, 1 = ponedeljak, ..., 6 = subota
+            let dayOfWeek = moment(date).format("d");
+
+            // trazimo koji je prvi sledeci dan kada je taj dan u nedelji, pocev od danas (ukljucujuci i danas)
+            let choice = moment();
+            while (choice.format("d") !== dayOfWeek) {
+                choice.add(1, "day");
+            }
+
+            // u niz guramo 4 datuma
+            for (let i = 0; i < 4; i++) {
+                ret.push(choice.clone());
+                choice.add(1, "week");
+            }
+        }
+        this.dateChoices = ret;
+        return ret;
+    }
+
+    cancelClass() {
+        alert('Testirati da li ovo ima veze s mozgom');
+        this._service.cancelClass(this.groupId, this.title, this.content, this.weekNumber);
     }
 }
