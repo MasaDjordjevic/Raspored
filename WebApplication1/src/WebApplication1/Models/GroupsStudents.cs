496abf5624e262f6f6f47289e136c672bcc6a6d7
Plavusa <masadordevic@gmail.com>
2016-06-25 10:07:22 +0200

    izmena baze > studentsgroups

--- WebApplication1/src/WebApplication1/Models/GroupsStudents.cs
+++ WebApplication1/src/WebApplication1/Models/GroupsStudents.cs
@@ -6,7 +6,9 @@ namespace WebApplication1.Models
     public partial class GroupsStudents
     {
         public int groupsStudentID { get; set; }
+        public bool? alert { get; set; }
         public int groupID { get; set; }
+        public bool? ignore { get; set; }
         public int studentID { get; set; }
 
         public virtual Groups group { get; set; }
