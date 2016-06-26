b3044fbf6d2feded2b2a0ee6d8e0c4707e62c9ac
Lazar Ljubenović <lazar.ljubenovic.1995@gmail.com>
2016-06-26 18:51:41 +0200

    Design: Class

--- WebApplication1/src/WebApplication1/wwwroot/app/timetable/r-timetable-class.component.ts
+++ WebApplication1/src/WebApplication1/wwwroot/app/timetable/r-timetable-class.component.ts
@@ -15,21 +15,10 @@ import {BulletinBoardComponent} from "../student-panel/bulletin-board/bulletin-b
     templateUrl: 'app/timetable/r-timetable-class.html',
     styleUrls: ['app/timetable/r-timetable-class.css'],
     host: {
-        "[class.red]": "color === 'MaterialRed'",
-        "[class.yellow]": "color === 'MaterialYellow'",
-        "[class.blue]": "color === 'MaterialBlue'",
-        "[class.orange]": "color === 'MaterialOrange'",
-        "[class.purple]": "color === 'MaterialPurple'",
-        "[class.green]": "color === 'MaterialGreen'",
-        "[class.active]": "active",
-        "[class.expanded]": "expanded",
-        "[style.left]": "expanded ? expandedLeft : ''",
-        "[style.top]": "expanded ? expandedTop : ''",
-        "[style.width]": "expanded ? expandedWidth : ''",
-        "[style.height]": "expanded ? expandedHeight : ''",
-        "[style.boxShadow]": "expanded ? '0 0 0 0.7em ' + color + ' inset' : ''",
-        "[style.color]": "expanded ? 'black' : textColor",
-        "(click)": "expand($event)",
+        "[style.left]": "expanded ? expandedLeftHost : '0'",
+        "[style.top]": "expanded ? expandedTopHost : '0'",
+        "[style.width]": "expanded ? expandedWidthHost : '100%'",
+        "[style.height]": "expanded ? expandedHeightHost : '100%'"
     },
     pipes: [ToTimestampPipe],
     directives: [R_BUTTON, R_DIALOG, AddActivityComponent, CancelClassComponent, AddTaskComponent, BulletinBoardComponent]
@@ -72,22 +61,36 @@ export class TimetableClassComponent {
         }
     }
 
+
     private expanded: boolean = false;
     private expandedLeft: any;
     private expandedTop: any;
     private expandedWidth: any;
     private expandedHeight: any;
 
+    private expandedLeftHost: any;
+    private expandedTopHost: any;
+    private expandedWidthHost: any;
+    private expandedHeightHost: any;
+
+    private selectedTab: number = 1;
+
     public expand($event = null) {
         // Da se ne pozove expandovanje odmah nakon collapse() klikom an dugme. Hakic.
-        if ($event && $event.target.nodeName === "BUTTON") return;
-        if (this.expanded) return;
+        if ($event && $event.target.nodeName === "BUTTON") {
+            return;
+        }
+
+        if (this.expanded) {
+            return;
+        }
+
         this.expanded = true;
 
         let ratio = .95;
-        let margin = (1 - ratio) / 2;
 
-        // Width i height
+        // Racunamo sirinu i visinu, koliko bi mogli da budu pod uslovom da hocemo
+        // da budu .95 maksimalne velicine
         this.expandedWidth = (this.elementRef.nativeElement.parentElement.parentElement.parentElement.parentElement
                 .getBoundingClientRect().width - 20); // -20 jer ne smem da predjem u scrollbar
         this.expandedWidth *= ratio;
@@ -95,21 +98,44 @@ export class TimetableClassComponent {
                 .getBoundingClientRect().height);
         this.expandedHeight *= ratio;
 
-        // Left i top
-        this.expandedLeft =
-            - (+this.elementRef.nativeElement.getBoundingClientRect().left -
-            +this.elementRef.nativeElement.parentElement.parentElement.parentElement.parentElement.getBoundingClientRect().left);
-        this.expandedLeft += this.expandedWidth * margin;
-        this.expandedTop =
-            - (this.elementRef.nativeElement.getBoundingClientRect().top -
-            this.elementRef.nativeElement.parentElement.parentElement.parentElement.parentElement.getBoundingClientRect().top);
-        this.expandedTop += this.expandedHeight * margin;
+        // Ceo raspored
+        let parentRect = this.elementRef.nativeElement
+            .parentElement.parentElement.parentElement.parentElement.getBoundingClientRect();
+        // Ovaj element
+        let thisRect = this.elementRef.nativeElement.getBoundingClientRect();
+
+        // Dolazimo do gornjeg levog ugla
+        this.expandedLeftHost = (+parentRect.left - +thisRect.left);
+        this.expandedTopHost = (+parentRect.top - +thisRect.top);
+
+        if (this.expandedWidth < 480 || this.expandedHeight < 520) {
+            // Ako je izracunata vrednost premala, ipak idemo od ivice do ivice rasporeda i ne stavljamo margine.
+            this.expandedWidth /= ratio;
+            this.expandedHeight /= ratio;
+        } else {
+            // Ako ipak imamo dovoljno mesta za ono sto hocemo, nema potrebe da ga sirimo vise nego sto treba
+            this.expandedWidth = 480;
+            this.expandedHeight = 520;
+
+            // Pomeramo na sredinu
+            this.expandedLeft = (+parentRect.width - this.expandedWidth) / 2;
+            this.expandedTop = (+parentRect.height - this.expandedHeight) / 2;
+        }
+
+        // Svakako host terba da ide od ivice do ivice
+        this.expandedWidthHost = +parentRect.width - 20; // zbog scrollbara
+        this.expandedHeightHost = +parentRect.height;
 
         // dodavanje jedinica
         this.expandedLeft += "px";
         this.expandedTop += "px";
         this.expandedHeight += 'px';
         this.expandedWidth += 'px';
+        this.expandedLeftHost += "px";
+        this.expandedTopHost += "px";
+        this.expandedHeightHost += 'px';
+        this.expandedWidthHost += 'px';
+
     }
 
     public collapse() {
@@ -117,8 +143,11 @@ export class TimetableClassComponent {
     }
 
     public toggle() {
-        if (this.expanded) this.collapse();
-        else this.expand();
+        if (this.expanded) {
+            this.collapse();
+        } else {
+            this.expand();
+        }
     }
 
     // dodavanje u lični
